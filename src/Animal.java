5563c6d26dc0ddee902ac7a9f66429e51ba90229
Munarbek <149784826+Dayons@users.noreply.github.com>
2023-11-10 19:29:24 +0600

    first commit

---
+++ src/Animal.java
@@ -0,0 +1,9 @@
+public class Animal {
+    private String name;
+}
+//git init
+//git add README.md
+//git commit -m "first commit"
+//git branch -M main
+//git remote add origin https://github.com/Dayons/testGitProject.git
+//git push -u origin main
